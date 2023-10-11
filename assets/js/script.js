533529e295e18a10fd6cba55fa00e70bd008ed80
Riquelme <riquelmestayler@gmail.com>
2023-10-11 19:46:33 -0300

    Commit do projeto newlest login

---
+++ assets/js/script.js
@@ -0,0 +1,26 @@
+
+document.querySelector('#subscribe-button').addEventListener('click', verifyEmail)
+
+function verifyEmail(){
+    let input = document.querySelector('input')
+    let inputValue = input.value
+    let warning = document.querySelector('#warning')
+    if(inputValue != ''){
+        let regex =  /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        if(regex.test(inputValue)){
+            document.querySelector('.login-layout').style.display = 'none'
+            document.querySelector('.subscribing').style.display = 'block'
+        }else{
+            input.style.border = '1px solid red'
+            input.style.color = 'hsl(4, 100%, 67%)'
+            input.style.backgroundColor = 'hsl(4, 100%, 67%, 20%)'
+            warning.innerHTML = 'Valid email required'
+        }
+    }else{
+        warning.innerHTML = 'fill in the email'
+        input.style.border = '1px solid red'
+        input.style.color = 'hsl(4, 100%, 67%)'
+        input.style.backgroundColor = 'hsl(4, 100%, 67%, 20%)'
+            
+    }
+}
